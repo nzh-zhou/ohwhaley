a619dc6a600c512f6b49b2dcb321770d2eeeac7f
Ze Hong Zhou <nzh.zhou@outlook.com>
2021-10-08 10:43:01 +1100

    Create data phrases

---
+++ R/phrases.R
@@ -0,0 +1,8 @@
+#' Sample phrases for function `say`
+#'
+#' A character vector of sample phrases for the function `say`
+#'
+#' @format A character vector of phrases
+#'
+#' @source MQ Uni STAT1378 Tutorial Week 6
+"phrases"
