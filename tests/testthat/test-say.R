910ed734697a87893c015e00ae92003face3872d
Ze Hong Zhou <nzh.zhou@outlook.com>
2021-10-08 10:57:00 +1100

    Create license and edit DESCRIPTION

--- tests/testthat/test-say.R
+++ tests/testthat/test-say.R
@@ -1,3 +1,13 @@
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  # test the input is in correct format
+  what <- c("This is a character string")
+  say(what)
+  expect_type(what, "character")
+  expect_gt(length(what), 0)
+
+  # test the output is of correct format
+  expect_length(say(), 0) #Returns something with a length of 0
+  expect_null(say()) #Returns null
+  expect_invisible(say()) #Returns invisibly
+  expect_message(say()) #Returns a message
 })
